707a6a86d774d50e3c3572b4b478d05b51217258
Robert Mikhayelyan <romikh@microsoft.com>
2018-06-22 13:46:21 -0700

    Basic composition

---
+++ Win32CaptureSample/main.cpp
@@ -0,0 +1,124 @@
+#include "pch.h"
+
+using namespace winrt;
+using namespace Windows::UI;
+using namespace Windows::UI::Composition;
+using namespace Windows::UI::Composition::Desktop;
+
+auto CreateDispatcherQueueController()
+{
+    namespace abi = ABI::Windows::System;
+
+    DispatcherQueueOptions options
+    {
+        sizeof(DispatcherQueueOptions),
+        DQTYPE_THREAD_CURRENT,
+        DQTAT_COM_STA
+    };
+
+    Windows::System::DispatcherQueueController controller{ nullptr };
+    check_hresult(CreateDispatcherQueueController(options, reinterpret_cast<abi::IDispatcherQueueController**>(put_abi(controller))));
+    return controller;
+}
+
+DesktopWindowTarget CreateDesktopWindowTarget(Compositor const& compositor, HWND window)
+{
+    namespace abi = ABI::Windows::UI::Composition::Desktop;
+
+    auto interop = compositor.as<abi::ICompositorDesktopInterop>();
+    DesktopWindowTarget target{ nullptr };
+    check_hresult(interop->CreateDesktopWindowTarget(window, true, reinterpret_cast<abi::IDesktopWindowTarget**>(put_abi(target))));
+    return target;
+}
+
+int CALLBACK WinMain(
+    HINSTANCE instance,
+    HINSTANCE previousInstance,
+    LPSTR     cmdLine,
+    int       cmdShow);
+
+LRESULT CALLBACK WndProc(
+    HWND   hwnd,
+    UINT   msg,
+    WPARAM wParam,
+    LPARAM lParam);
+
+int CALLBACK WinMain(
+    HINSTANCE instance,
+    HINSTANCE previousInstance,
+    LPSTR     cmdLine,
+    int       cmdShow)
+{
+    // Create the window
+    WNDCLASSEX wcex = {};
+    wcex.cbSize = sizeof(WNDCLASSEX);
+    wcex.style = CS_HREDRAW | CS_VREDRAW;
+    wcex.lpfnWndProc = WndProc;
+    wcex.cbClsExtra = 0;
+    wcex.cbWndExtra = 0;
+    wcex.hInstance = instance;
+    wcex.hIcon = LoadIcon(instance, MAKEINTRESOURCE(IDI_APPLICATION));
+    wcex.hCursor = LoadCursor(NULL, IDC_ARROW);
+    wcex.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
+    wcex.lpszMenuName = NULL;
+    wcex.lpszClassName = L"Win32CaptureSample";
+    wcex.hIconSm = LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_APPLICATION));
+    WINRT_ASSERT(RegisterClassEx(&wcex));
+
+    HWND hwnd = CreateWindow(
+        L"Win32CaptureSample",
+        L"Win32CaptureSample",
+        WS_OVERLAPPEDWINDOW,
+        CW_USEDEFAULT,
+        CW_USEDEFAULT,
+        800,
+        600,
+        NULL,
+        NULL,
+        instance,
+        NULL);
+    WINRT_ASSERT(hwnd);
+
+    ShowWindow(hwnd, cmdShow);
+    UpdateWindow(hwnd);
+
+    // Create a dispatcher queue for our thread
+    auto controller = CreateDispatcherQueueController();
+
+    // Initialize Composition
+    auto compositor = Compositor();
+    auto target = CreateDesktopWindowTarget(compositor, hwnd);
+    auto root = compositor.CreateSpriteVisual();
+    root.RelativeSizeAdjustment({ 1.0f, 1.0f });
+    root.Brush(compositor.CreateColorBrush(Colors::CornflowerBlue()));
+    target.Root(root);
+
+    // Message pump
+    MSG msg;
+    while (GetMessage(&msg, NULL, 0, 0))
+    {
+        TranslateMessage(&msg);
+        DispatchMessage(&msg);
+    }
+
+    return msg.wParam;
+}
+
+LRESULT CALLBACK WndProc(
+    HWND   hwnd,
+    UINT   msg,
+    WPARAM wParam,
+    LPARAM lParam)
+{
+    switch (msg)
+    {
+    case WM_DESTROY:
+        PostQuitMessage(0);
+        break;
+    default:
+        return DefWindowProc(hwnd, msg, wParam, lParam);
+        break;
+    }
+
+    return 0;
+}
---
+++ Win32CaptureSample/pch.h
@@ -0,0 +1,9 @@
+#pragma once
+
+// WinRT
+#include <winrt/Windows.System.h>
+#include <winrt/Windows.UI.h>
+#include <winrt/Windows.UI.Composition.h>
+#include <winrt/Windows.UI.Composition.Desktop.h>
+#include <windows.ui.composition.interop.h>
+#include <DispatcherQueue.h>
