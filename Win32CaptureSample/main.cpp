1925ec6063f1ba63ed6c85f6608ed7e12fde859d
Robert Mikhayelyan <rob.mikh@outlook.com>
2019-06-22 17:48:01 -0700

    working sample window struct

--- Win32CaptureSample/main.cpp
+++ Win32CaptureSample/main.cpp
@@ -1,28 +1,152 @@
 #include "pch.h"
+#include "DesktopWindow.h"
 #include "App.h"
 #include "SimpleCapture.h"
 #include "Win32WindowEnumeration.h"
 
 using namespace winrt;
+using namespace Windows::Graphics::Capture;
 using namespace Windows::UI;
 using namespace Windows::UI::Composition;
+using namespace Windows::UI::Composition::Desktop;
 
-int CALLBACK WinMain(
-    HINSTANCE instance,
-    HINSTANCE previousInstance,
-    LPSTR     cmdLine,
-    int       cmdShow);
-
+// Globals
 auto g_app = std::make_shared<App>();
 auto g_windows = EnumerateWindows();
 
-LRESULT CALLBACK WndProc(
-    HWND   hwnd,
-    UINT   msg,
-    WPARAM wParam,
-    LPARAM lParam);
+struct SampleWindow : DesktopWindow<SampleWindow>
+{
+    SampleWindow(HINSTANCE instance, int cmdShow) noexcept
+    {
+        WNDCLASSEX wcex = {};
+        wcex.cbSize = sizeof(WNDCLASSEX);
+        wcex.style = CS_HREDRAW | CS_VREDRAW;
+        wcex.lpfnWndProc = WndProc;
+        wcex.cbClsExtra = 0;
+        wcex.cbWndExtra = 0;
+        wcex.hInstance = instance;
+        wcex.hIcon = LoadIcon(instance, MAKEINTRESOURCE(IDI_APPLICATION));
+        wcex.hCursor = LoadCursor(NULL, IDC_ARROW);
+        wcex.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
+        wcex.lpszMenuName = NULL;
+        wcex.lpszClassName = L"Win32CaptureSample";
+        wcex.hIconSm = LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_APPLICATION));
+        WINRT_VERIFY(RegisterClassEx(&wcex));
+        WINRT_ASSERT(!m_window);
 
-int CALLBACK WinMain(
+        WINRT_VERIFY(CreateWindow(
+            L"Win32CaptureSample",
+            L"Win32CaptureSample",
+            WS_OVERLAPPEDWINDOW,
+            CW_USEDEFAULT,
+            CW_USEDEFAULT,
+            800,
+            600,
+            NULL,
+            NULL,
+            instance,
+            this));
+
+        WINRT_ASSERT(m_window);
+
+        ShowWindow(m_window, cmdShow);
+        UpdateWindow(m_window);
+
+        CreateControls(instance);
+    }
+
+    DesktopWindowTarget CreateWindowTarget(Compositor const& compositor)
+    {
+        return CreateDesktopWindowTarget(compositor, m_window, true);
+    }
+
+    void Initialize(winrt::Windows::Foundation::IUnknown const& object)
+    {
+        auto initializer = object.as<IInitializeWithWindow>();
+        winrt::check_hresult(initializer->Initialize(m_window));
+    }
+
+    LRESULT MessageHandler(UINT const message, WPARAM const wparam, LPARAM const lparam) noexcept
+    {
+        switch (message)
+        {
+        case WM_COMMAND:
+            {
+                auto command = HIWORD(wparam);
+                switch (command)
+                {
+                case CBN_SELCHANGE:
+                {
+                    auto index = SendMessage((HWND)lparam, CB_GETCURSEL, 0, 0);
+                    auto window = g_windows[index];
+                    g_app->StartCapture(window.Hwnd());
+                }
+                break;
+                case BN_CLICKED:
+                {
+                    auto ignored = g_app->StartCaptureWithPickerAsync();
+                }
+                break;
+                }
+            }
+            break;
+        default:
+            return base_type::MessageHandler(message, wparam, lparam);
+            break;
+        }
+
+        return 0;
+    }
+
+private:
+    void CreateControls(HINSTANCE instance)
+    {
+        // Create combo box
+        HWND comboBoxHwnd = CreateWindow(
+            WC_COMBOBOX,
+            L"",
+            CBS_DROPDOWNLIST | CBS_HASSTRINGS | WS_VSCROLL | WS_CHILD | WS_OVERLAPPED | WS_VISIBLE,
+            10,
+            10,
+            200,
+            200,
+            m_window,
+            NULL,
+            instance,
+            NULL);
+        WINRT_VERIFY(comboBoxHwnd);
+
+        // Populate combo box
+        for (auto& window : g_windows)
+        {
+            SendMessage(comboBoxHwnd, CB_ADDSTRING, 0, (LPARAM)window.Title().c_str());
+        }
+
+        // Create button
+        HWND buttonHwnd = CreateWindow(
+            WC_BUTTON,
+            L"Use Picker",
+            WS_TABSTOP | WS_VISIBLE | WS_CHILD | BS_DEFPUSHBUTTON,
+            10,
+            40,
+            200,
+            30,
+            m_window,
+            NULL,
+            instance,
+            NULL);
+        WINRT_VERIFY(buttonHwnd);
+
+        m_comboBoxHwnd = comboBoxHwnd;
+        m_buttonHwnd = buttonHwnd;
+    }
+
+private:
+    HWND m_comboBoxHwnd = NULL;
+    HWND m_buttonHwnd = NULL;
+};
+
+int __stdcall WinMain(
     HINSTANCE instance,
     HINSTANCE previousInstance,
     LPSTR     cmdLine,
@@ -44,86 +168,22 @@ int CALLBACK WinMain(
         return 1;
     }
 
-    // Create the window
-    WNDCLASSEX wcex = {};
-    wcex.cbSize = sizeof(WNDCLASSEX);
-    wcex.style = CS_HREDRAW | CS_VREDRAW;
-    wcex.lpfnWndProc = WndProc;
-    wcex.cbClsExtra = 0;
-    wcex.cbWndExtra = 0;
-    wcex.hInstance = instance;
-    wcex.hIcon = LoadIcon(instance, MAKEINTRESOURCE(IDI_APPLICATION));
-    wcex.hCursor = LoadCursor(NULL, IDC_ARROW);
-    wcex.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
-    wcex.lpszMenuName = NULL;
-    wcex.lpszClassName = L"Win32CaptureSample";
-    wcex.hIconSm = LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_APPLICATION));
-    WINRT_VERIFY(RegisterClassEx(&wcex));
-
-    HWND hwnd = CreateWindow(
-        L"Win32CaptureSample",
-        L"Win32CaptureSample",
-        WS_OVERLAPPEDWINDOW,
-        CW_USEDEFAULT,
-        CW_USEDEFAULT,
-        800,
-        600,
-        NULL,
-        NULL,
-        instance,
-        NULL);
-    WINRT_VERIFY(hwnd);
-
-    ShowWindow(hwnd, cmdShow);
-    UpdateWindow(hwnd);
-
-    // Create combo box
-    HWND comboBoxHwnd = CreateWindow(
-        WC_COMBOBOX,
-        L"",
-        CBS_DROPDOWNLIST | CBS_HASSTRINGS | WS_VSCROLL | WS_CHILD | WS_OVERLAPPED | WS_VISIBLE,
-        10,
-        10,
-        200,
-        200,
-        hwnd,
-        NULL,
-        instance,
-        NULL);
-    WINRT_VERIFY(comboBoxHwnd);
-
-    // Populate combo box
-    for (auto& window : g_windows)
-    {
-        SendMessage(comboBoxHwnd, CB_ADDSTRING, 0, (LPARAM)window.Title().c_str());
-    }
-
-    // Create button
-    HWND buttonHwnd = CreateWindow(
-        WC_BUTTON,
-        L"Use Picker",
-        WS_TABSTOP | WS_VISIBLE | WS_CHILD | BS_DEFPUSHBUTTON,
-        10,
-        40,
-        200,
-        30,
-        hwnd,
-        NULL,
-        instance,
-        NULL);
-    WINRT_VERIFY(buttonHwnd);
+    // Create Window
+    auto window = SampleWindow(instance, cmdShow);
 
     // Create a DispatcherQueue for our thread
     auto controller = CreateDispatcherQueueControllerForCurrentThread();
 
     // Initialize Composition
     auto compositor = Compositor();
-    auto target = CreateDesktopWindowTarget(compositor, hwnd, true);
+    auto target = window.CreateWindowTarget(compositor);
     auto root = compositor.CreateContainerVisual();
     root.RelativeSizeAdjustment({ 1.0f, 1.0f });
     target.Root(root);
 
-    auto picker = CreateCapturePickerForHwnd(hwnd);
+    // Create and initialize the picker
+    auto picker = GraphicsCapturePicker();
+    window.Initialize(picker);
 
     // Enqueue our capture work on the dispatcher
     auto queue = controller.DispatcherQueue();
@@ -142,43 +202,4 @@ int CALLBACK WinMain(
     }
 
     return msg.wParam;
-}
-
-LRESULT CALLBACK WndProc(
-    HWND   hwnd,
-    UINT   msg,
-    WPARAM wParam,
-    LPARAM lParam)
-{
-    switch (msg)
-    {
-    case WM_DESTROY:
-        PostQuitMessage(0);
-        break;
-    case WM_COMMAND:
-        {
-            auto command = HIWORD(wParam);
-            switch (command)
-            {
-            case CBN_SELCHANGE:
-                {
-                    auto index = SendMessage((HWND)lParam, CB_GETCURSEL, 0, 0);
-                    auto window = g_windows[index];
-                    g_app->StartCapture(window.Hwnd());
-                }
-                break;
-            case BN_CLICKED:
-                {
-                    auto ignored = g_app->StartCaptureWithPickerAsync();
-                }
-                break;
-            }
-        }
-        break;
-    default:
-        return DefWindowProc(hwnd, msg, wParam, lParam);
-        break;
-    }
-
-    return 0;
 }
