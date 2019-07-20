753bcbfae171dca711162c2361447e7b0406ceda
Chris Guzak <chris_guzak@msn.com>
2019-07-19 20:36:54 -0700

    use comctl32 v6 for better UI look

--- Win32CaptureSample/main.cpp
+++ Win32CaptureSample/main.cpp
@@ -3,6 +3,8 @@
 #include "App.h"
 #include "SampleWindow.h"
 
+#pragma comment(linker,"/manifestdependency:\"type='win32' name='Microsoft.Windows.Common-Controls' version='6.0.0.0' processorArchitecture='*' publicKeyToken='6595b64144ccf1df' language='*'\"")
+
 using namespace winrt;
 using namespace Windows::Graphics::Capture;
 using namespace Windows::UI::Composition;
