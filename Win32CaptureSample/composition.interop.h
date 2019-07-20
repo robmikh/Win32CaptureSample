71d7a027d04d67eefebae5682205a1dd5bfb0b99
Chris Guzak <chris_guzak@msn.com>
2019-07-19 22:50:44 -0700

    remove unneeded casts, reformat too

--- Win32CaptureSample/composition.interop.h
+++ Win32CaptureSample/composition.interop.h
@@ -3,28 +3,22 @@
 #include <windows.ui.composition.interop.h>
 #include <d2d1_1.h>
 
-inline auto CreateCompositionGraphicsDevice(
-    winrt::Windows::UI::Composition::Compositor const& compositor,
-    ::IUnknown* device)
+inline auto CreateCompositionGraphicsDevice(winrt::Windows::UI::Composition::Compositor const& compositor, ::IUnknown* device)
 {
     winrt::Windows::UI::Composition::CompositionGraphicsDevice graphicsDevice{ nullptr };
     auto compositorInterop = compositor.as<ABI::Windows::UI::Composition::ICompositorInterop>();
     winrt::com_ptr<ABI::Windows::UI::Composition::ICompositionGraphicsDevice> graphicsInterop;
     winrt::check_hresult(compositorInterop->CreateGraphicsDevice(device, graphicsInterop.put()));
     winrt::check_hresult(graphicsInterop->QueryInterface(winrt::guid_of<winrt::Windows::UI::Composition::CompositionGraphicsDevice>(),
-        reinterpret_cast<void**>(winrt::put_abi(graphicsDevice))));
+        winrt::put_abi(graphicsDevice)));
     return graphicsDevice;
 }
 
-inline void ResizeSurface(
-    winrt::Windows::UI::Composition::CompositionDrawingSurface const& surface,
+inline void ResizeSurface(winrt::Windows::UI::Composition::CompositionDrawingSurface const& surface,
     winrt::Windows::Foundation::Size const& size)
 {
     auto surfaceInterop = surface.as<ABI::Windows::UI::Composition::ICompositionDrawingSurfaceInterop>();
-    SIZE newSize = {};
-    newSize.cx = static_cast<LONG>(std::round(size.Width));
-    newSize.cy = static_cast<LONG>(std::round(size.Height));
-    winrt::check_hresult(surfaceInterop->Resize(newSize));
+    winrt::check_hresult(surfaceInterop->Resize({ static_cast<LONG>(std::round(size.Width)), static_cast<LONG>(std::round(size.Height)) }));
 }
 
 // Do the type conversion from long -> float without warnings
@@ -33,46 +27,38 @@ inline D2D1::Matrix3x2F Translation(const POINT& pt)
     return D2D1::Matrix3x2F::Translation(static_cast<float>(pt.x), static_cast<float>(pt.y));
 }
 
-inline auto SurfaceBeginDraw(
-    winrt::Windows::UI::Composition::CompositionDrawingSurface const& surface)
+inline auto SurfaceBeginDraw(winrt::Windows::UI::Composition::CompositionDrawingSurface const& surface)
 {
     auto surfaceInterop = surface.as<ABI::Windows::UI::Composition::ICompositionDrawingSurfaceInterop>();
     winrt::com_ptr<ID2D1DeviceContext> context;
     POINT offset = {};
-    winrt::check_hresult(surfaceInterop->BeginDraw(nullptr, __uuidof(ID2D1DeviceContext), context.put_void(), &offset));
+    winrt::check_hresult(surfaceInterop->BeginDraw(nullptr, __uuidof(context), context.put_void(), &offset));
     context->SetTransform(Translation(offset));
     return context;
 }
 
-inline void SurfaceEndDraw(
-    winrt::Windows::UI::Composition::CompositionDrawingSurface const& surface)
+inline void SurfaceEndDraw(winrt::Windows::UI::Composition::CompositionDrawingSurface const& surface)
 {
     auto surfaceInterop = surface.as<ABI::Windows::UI::Composition::ICompositionDrawingSurfaceInterop>();
     winrt::check_hresult(surfaceInterop->EndDraw());
 }
 
-inline auto CreateCompositionSurfaceForSwapChain(
-    winrt::Windows::UI::Composition::Compositor const& compositor,
-    ::IUnknown* swapChain)
+inline auto CreateCompositionSurfaceForSwapChain(winrt::Windows::UI::Composition::Compositor const& compositor, ::IUnknown* swapChain)
 {
     winrt::Windows::UI::Composition::ICompositionSurface surface{ nullptr };
     auto compositorInterop = compositor.as<ABI::Windows::UI::Composition::ICompositorInterop>();
     winrt::com_ptr<ABI::Windows::UI::Composition::ICompositionSurface> surfaceInterop;
     winrt::check_hresult(compositorInterop->CreateCompositionSurfaceForSwapChain(swapChain, surfaceInterop.put()));
-    winrt::check_hresult(surfaceInterop->QueryInterface(winrt::guid_of<winrt::Windows::UI::Composition::ICompositionSurface>(),
-        reinterpret_cast<void**>(winrt::put_abi(surface))));
+    winrt::check_hresult(surfaceInterop->QueryInterface(winrt::guid_of<winrt::Windows::UI::Composition::ICompositionSurface>(), winrt::put_abi(surface)));
     return surface;
 }
 
-inline auto CreateDesktopWindowTarget(
-    winrt::Windows::UI::Composition::Compositor const& compositor,
-    HWND window,
-    bool isTopMost)
+inline auto CreateDesktopWindowTarget(winrt::Windows::UI::Composition::Compositor const& compositor, HWND window, bool isTopMost)
 {
     namespace abi = ABI::Windows::UI::Composition::Desktop;
 
     auto interop = compositor.as<abi::ICompositorDesktopInterop>();
     winrt::Windows::UI::Composition::Desktop::DesktopWindowTarget target{ nullptr };
-    winrt::check_hresult(interop->CreateDesktopWindowTarget(window, isTopMost, reinterpret_cast<abi::IDesktopWindowTarget * *>(winrt::put_abi(target))));
+    winrt::check_hresult(interop->CreateDesktopWindowTarget(window, isTopMost, reinterpret_cast<abi::IDesktopWindowTarget**>(winrt::put_abi(target))));
     return target;
 }
