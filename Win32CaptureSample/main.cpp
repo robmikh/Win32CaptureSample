1ecf38217f3f48c9be76cf22e2609e98ab1b6aa1
robmikh <rob.mikh@outlook.com>
2024-06-08 19:26:01 -0700

    move to app manifest

--- Win32CaptureSample/main.cpp
+++ Win32CaptureSample/main.cpp
@@ -2,8 +2,6 @@
 #include "App.h"
 #include "SampleWindow.h"
 
-#pragma comment(linker,"/manifestdependency:\"type='win32' name='Microsoft.Windows.Common-Controls' version='6.0.0.0' processorArchitecture='*' publicKeyToken='6595b64144ccf1df' language='*'\"")
-
 namespace winrt
 {
     using namespace Windows::Graphics::Capture;
